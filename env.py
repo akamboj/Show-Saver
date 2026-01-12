8abf9d265306fde15469ff60cc8e0c8ed1d9cf9c
Anoop Kamboj <7720575+akamboj@users.noreply.github.com>
2026-01-11 20:45:03 -0800

    File to manage environment vars and volumes

---
+++ env.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+#
+# Directories
+#
+
+CONFIG_DIR = Path(os.getenv("CONFIG_DIR", "/config"))
+SHOW_DIR = Path(os.getenv("SHOW_DIR", "/tvshows"))
+TMP_DIR = Path(os.getenv("TMP_DIR", "/tmp"))
+
+
+#
+# User info vars
+#
+
+URL = os.getenv("URL", "")
+USERNAME = os.getenv("USERNAME", "")
+PASSWORD = os.getenv("PASSWORD", "")
