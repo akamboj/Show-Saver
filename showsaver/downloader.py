dbe47467d5c689d88777bebb766a2e67ab426ec8
Anoop Kamboj <7720575+akamboj@users.noreply.github.com>
2026-05-01 20:46:49 -0700

    chore: drop world writable bit from downloaded file perms

--- showsaver/downloader.py
+++ showsaver/downloader.py
@@ -207,10 +207,10 @@ def copy_to_destination(info_dict, show_path, base_destination_path, processor=N
 
     print(f'Starting copy from: {show_path}, to: {full_destination_path}')
     os.makedirs(full_destination_path, exist_ok=True)
-    os.chmod(full_destination_path, 0o777)
+    os.chmod(full_destination_path, 0o775)
 
     shutil.copy2(show_path, full_episode_path)
-    os.chmod(full_episode_path, 0o666)
+    os.chmod(full_episode_path, 0o664)
     print("Copy complete!")
 
 
