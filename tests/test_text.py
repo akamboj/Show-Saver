e64ef9ae7e35185bd2b03d825629ac00285fd64c
Anoop Kamboj <7720575+akamboj@users.noreply.github.com>
2026-05-26 22:47:40 -0700

    Add test for expected compat filename from ytdlp

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,3 +1,5 @@
+import yt_dlp
+
 from showsaver.text import normalize_title
 
 
@@ -12,3 +14,19 @@ def test_normalize_title_replaces_fullwidth_double_quote_char():
 def test_normalize_title_leaves_other_titles_unchanged():
     title = 'Some \'Quoted\' Title'
     assert normalize_title(title) == title
+
+
+def test_normalize_title_handles_fullwidth_quotes_left_by_ytdlp_filename_sanitization():
+    ydl = yt_dlp.YoutubeDL({
+        'compat_opts': {'filename-sanitization'},
+        'outtmpl': '%(title)s.%(ext)s',
+        'quiet': True,
+    })
+
+    filename = ydl.prepare_filename({
+        'title': 'Some \uff02Quoted\uff02 Title',
+        'ext': 'mkv',
+    })
+
+    assert filename == 'Some \uff02Quoted\uff02 Title.mkv'
+    assert normalize_title(filename) == 'Some \'Quoted\' Title.mkv'
