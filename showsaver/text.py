a81628311a7774e3181ea1b7f9c4e0f79c43af6b
Anoop Kamboj <7720575+akamboj@users.noreply.github.com>
2026-06-25 22:21:14 -0700

    fix: strip question marks from titles explicitly

--- showsaver/text.py
+++ showsaver/text.py
@@ -5,4 +5,5 @@ FULLWIDTH_DOUBLE_QUOTE = '\uff02'
 def normalize_title(title: str) -> str:
     title = title.replace(FULLWIDTH_DOUBLE_QUOTE, '\'')
     title = title.replace(DOUBLE_QUOTE, '\'')
+    title = title.replace('?', '')
     return title
